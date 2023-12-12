38ce9a6a755e5381e7fee3e02c576055eb193e89
S48AHan <saberahmedn2@gmail.com>
2023-12-12 10:41:19 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function game(){
+    alert("play dota2!");
+}
+days = 7;
+function play(){
+    var matches = 1000;
+    alert("Don't Play!"+"\nMatch count: "+matches +" in "+days+" days");
+}
+
+var money = 10 ;
+var name  = "S48Ahan";
